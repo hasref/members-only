0023a49e3c1591a38f784be8e9418c0eed0e9572
hasan ashraf <hasan_ashraf@fastmail.com>
2021-07-23 02:27:43 +0200

    add sign up form

---
+++ controllers/signup.js
@@ -0,0 +1,7 @@
+exports.signUp = (req, res, next) => {
+  res.render('signupForm');
+};
+
+exports.createUser = (req, res, next) => {
+  res.send('Not done yet');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const signUpController = require('../controllers/signup');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/signup', signUpController.signUp);
 
 module.exports = router;
